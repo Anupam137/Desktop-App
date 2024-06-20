0b00396aa52357582f474bb492b5c14dc2dd6b1d
Anupam137 <131847192+Anupam137@users.noreply.github.com>
2024-06-20 22:11:21 +0530

    Add files via upload

---
+++ MainForm1.Designer.vb
@@ -0,0 +1,49 @@
+﻿Partial Class MainForm1
+    Inherits System.Windows.Forms.Form
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.btnViewSubmissions = New System.Windows.Forms.Button()
+        Me.btnCreateNewSubmission = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'btnViewSubmissions
+        '
+        Me.btnViewSubmissions.Location = New System.Drawing.Point(12, 12)
+        Me.btnViewSubmissions.Name = "btnViewSubmissions"
+        Me.btnViewSubmissions.Size = New System.Drawing.Size(156, 23)
+        Me.btnViewSubmissions.TabIndex = 0
+        Me.btnViewSubmissions.Text = "View Submissions (&V)"
+        Me.btnViewSubmissions.UseVisualStyleBackColor = True
+        '
+        'btnCreateNewSubmission
+        '
+        Me.btnCreateNewSubmission.Location = New System.Drawing.Point(12, 41)
+        Me.btnCreateNewSubmission.Name = "btnCreateNewSubmission"
+        Me.btnCreateNewSubmission.Size = New System.Drawing.Size(156, 23)
+        Me.btnCreateNewSubmission.TabIndex = 1
+        Me.btnCreateNewSubmission.Text = "Create New Submission (&C)"
+        Me.btnCreateNewSubmission.UseVisualStyleBackColor = True
+        '
+        'MainForm1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 81)
+        Me.Controls.Add(Me.btnCreateNewSubmission)
+        Me.Controls.Add(Me.btnViewSubmissions)
+        Me.Name = "MainForm1"
+        Me.Text = "Main Form"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents btnViewSubmissions As Button
+    Friend WithEvents btnCreateNewSubmission As Button
+End Class
---
+++ MainForm1.vb
@@ -0,0 +1,13 @@
+﻿Public Class MainForm1
+    Private Sub btnViewSubmissions_Click(sender As Object, e As EventArgs) Handles btnViewSubmissions.Click
+        ' Open the ViewSubmissionsForm
+        Dim viewForm As New ViewSubmissionsForm()
+        viewForm.Show()
+    End Sub
+
+    Private Sub btnCreateNewSubmission_Click(sender As Object, e As EventArgs) Handles btnCreateNewSubmission.Click
+        ' Open the CreateSubmissionForm
+        Dim createForm As New CreateSubmissionForm()
+        createForm.Show()
+    End Sub
+End Class
